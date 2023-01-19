fd2d07924f45172ab3323e70fccfc364631dcab6
sumitpahadi <sumitrawat1102000@gmai.com>
2023-01-19 14:37:55 +0530

    tour booking website

---
+++ travel website/about.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/about.jpg) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <p>Honest prices – we have the best prices for tours, and a huge selection of destinations will not leave indifferent even avid tourist.
+
+Reliable partners – We work with the best tour operators who over the years have proven their professionalism.
+
+Full Service – Everything that a tourist may need when going abroad, in addition to the ticket.
+
+Quality Guarantee – We do not have to worry about paperwork necessary documents.
+
+Full support – contacting us, you get full support, from choosing the tour and ending with advice on what to take with you on a trip.
+
+Document processing – With us you won’t have to worry about processing the necessary documents.</p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>top destinations</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 guide service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading-title"> clients reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>It was a great experience and awesome service by Sky World Tour & Travels. We travelled to Bali with them and I must say that it was the best trip of my life. They helped us with each and every small issue and were always there for assistance. We would love to travel with them in the future.</p>
+            <h3>keshav Gaur</h3>
+            <span>traveler</span>
+            <img src="images/pic-1.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>We were so impressed over the architecture, the culture and the food, especially the incredible buffet at the Palace Emirates 7-star hotel! Our tour guide, Marshall, was also so informative!. I highly recommend Abu Dhabi to future travelers as well!! Many thanks for some marvelous memories!</p>
+            <h3>Shivani Nautiyal</h3>
+            <span>traveler</span>
+            <img src="images/pic-2.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Wonderful trip! Every detail went as planned from the hotels to car rentals to train scheduling. The day to day itinerary worked perfectly. Although we have traveled to France many times, this was an exciting experience.. Highly recommended!We had a very memorable day trip to Loire Valley.</p>
+            <h3>Karan Rawat</h3>
+            <span>traveler</span>
+            <img src="images/pic-3.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae cum quibusdam odio dignissimos nemo?</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-4.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui distinctio possimus?</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-5.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui distinctio possimus?</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-6.png" alt="">
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 91+9625613067 </a>
+         <a href="#"> <i class="fas fa-phone"></i>  91+9625613067 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> sumitrawat1102000@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> utttar pradesh, india - 2001009 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100066165454663"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <!-- <a href="#"> <i class="fab fa-twitter"></i> twitter </a> -->
+         <a href="https://www.instagram.com/sumitrawat6216/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/sumit-rawat-419b8321b/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span> sumit rawat</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ travel website/package.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/package.jpg) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/dubai.jpg" alt="">
+         </div>
+         <div class="content">
+         <h3>Dubai, United Arab Emirates</h3>
+            <p>Dubai is regarded as one of the best cities in the United Arab Emirates. It is renowned for many different reasons, mainly for being one of the most luxurious cities, with ultra-modern buildings and architecture and, of course, the Burj Khalifa, the tallest building in the world (828m!).</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/grecce.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Crete, Greece</h3>
+            <p>The largest island in Greece, Crete is a diverse and vibrant land packed with ancient ruins, buzzing cities and breathtaking beaches. Many people come here for the sun, sea and sand yet the best things to do in Crete include much more than the standard package vacation.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/london.jpg" alt="">
+         </div>
+         <div class="content">
+              <h3>London,United Kingdom</h3>
+            <p>Apart from Big Ben and Buckingham Palace, London is famous for its status as one of the world's greatest cosmopolitan cities, with an incredible array of museums, shops, and restaurants. It's also known for its interesting history in regards to royalty, politics, arts, science, and architecture</p>
+         
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/mexico.jpg" alt="">
+         </div>
+         <div class="content">
+        <h3>Cancún, Mexico</h3>
+            <p>Mexico is known for its rich culture, ancient ruins, dazzling beaches, and incredible cuisine. Tour Mayan temple ruins by day and indulge in fantastic food while listening to the rhythms of live music by night. Lounge on tropical beaches and explore the vibrant corals and marine life of the underwater world. </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Bali, Indonesia</h3>
+            <p>Also known as the Land of the Gods, Bali appeals through its sheer natural beauty of looming volcanoes and lush terraced rice fields that exude peace and serenity. It is also famous for surfers' paradise.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/italy.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Rome, Italy</h3>
+            <p>Rome, the “Eternal City,” brims with ancient history, from the Colosseum to the port of Ostia Antica to majestic Vatican City and the Sistine Chapel. Because of its history, art, architecture, and beauty and perhaps its gelato and pasta!Rome is one of our most popular cities.!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/istanbul.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Istanbul, Turkey </h3>
+            <p>Istanbul has a timeless charm that owes much to its rich history. The city was historically referred to as Byzantium and Constantinople. It served as a focal point of several ancient empires. Numerous architectural wonders, remnants of these empires, still stand tall in the heart of the city.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/paris.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Paris, France</h3>
+            <p>Paris is known for its gorgeous, imposing monuments. These iconic structures, often an exemplar of a particular era in architecture, are one of the city's instantly recognizable elements. The preeminent of Paris's landmarks is the Eiffel Tower.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/egypt.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Hurghada, Egypt</h3>
+            <p>Since the city is located next to the Red Sea, it attracts numerous tourists each year. Tourists come here to relax on the beaches, swim, snorkel, and scuba. The most famous resort on in the city is the resort of Hurghada, although there are nearly hundred hotels along the shoreline.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/vegas.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Las Vegas, Nevada</h3>
+            <p>Las Vegas is an internationally renowned major resort city, known primarily for its gambling, shopping, fine dining, entertainment, and nightlife. The Las Vegas Valley as a whole serves as the leading financial, commercial, and cultural center for Nevada.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/hawaii.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> Oahu, Hawaii</h3>
+            <p>Oahu is the site of Pearl Harbor (where Japanese forces staged their first attack against the United States in World War II), Waikiki (with its world-famous beaches), and the North Shore (known for surfing).</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/new york.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3> New York City, New York</h3>
+            <p>Anchored by Wall Street in the Financial District of Lower Manhattan, New York City has been called both the world's leading financial center and the most powerful city in the world, and is home to the world's two largest stock exchanges by total market capitalization, the New York Stock Exchange and Nasdaq.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+      <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 91+9625613067 </a>
+         <a href="#"> <i class="fas fa-phone"></i>  91+9625613067 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> sumitrawat1102000@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> utttar pradesh, india - 2001009 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100066165454663"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <!-- <a href="#"> <i class="fab fa-twitter"></i> twitter </a> -->
+         <a href="https://www.instagram.com/sumitrawat6216/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/sumit-rawat-419b8321b/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>sumit rawat</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
